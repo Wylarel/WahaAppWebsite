e31ecff67a5255243815fc0ca10c6e755f6ac504
Wylarel <timjemine@gmail.com>
2020-08-31 16:22:11 +0200

    Add mobile

---
+++ ressources/js/mobile.js
@@ -0,0 +1,33 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+if( isMobile.any() ) {
+    $("#app").hide();
+    $("#mobile").show();
+} else {
+    $("#app").show();
+    $("#mobile").hide();
+}
+
+function continue_on_web() {
+    $("#app").show();
+    $("#mobile").hide();
+}  
